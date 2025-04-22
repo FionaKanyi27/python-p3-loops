1ed7172547a4fb7e0d4f4952a1aa52c533a3b116
Fiona <fionakanyi0@gmail.com>
2025-04-22 19:48:07 +0300

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
- i = 10
-while i > 0:
+    i = 10 
+    while i > 0:
         print(i)
         i -= 1  
-print("Happy New Year!")
+    print("Happy New Year!")
+
 
 def square_integers(int_list):
         return [x ** 2 for x in int_list]
